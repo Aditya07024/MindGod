b3ed45a11ca1d38cb19a16a2d88be342feb568de
Aditya07024 <adityakumar07024@gmail.com>
2026-05-09 07:53:52 +0530

    some fixes for Render

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,33 +1,59 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import {cloudflare} from "@cloudflare/vite-plugin";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    cloudflare(),
+    // cloudflare(),
     tanstackStart({
       server: { entry: "server" },
-    }),
+      react: {
+        customViteReactPlugin: true,
+      },
+    } as any),
     react(),
     tsconfigPaths(),
     tailwindcss(),
   ],
+
+  environments: {
+    tanstack_start_app: {
+      resolve: {
+        external: [],
+      },
+    },
+  },
+
   build: {
     target: "es2020",
-    rollupOptions: {},
-    // Reduce source map size in production
     sourcemap: false,
-    // Increase chunk size warning threshold
     chunkSizeWarningLimit: 1000,
     minify: true,
   },
-  server: {
-    middlewareMode: true,
-  },
+
   resolve: {
     dedupe: ["react", "react-dom"],
+  },
+
+  optimizeDeps: {
+    exclude: [
+      "@tanstack/start-server-core",
+      "@tanstack/react-start",
+      "@tanstack/react-start/server",
+      "@tanstack/react-start/client",
+      "@tanstack/react-router",
+    ],
+    esbuildOptions: {
+      external: [
+        "#tanstack-router-entry",
+        "#tanstack-start-entry",
+        "#tanstack-start-plugin-adapters",
+        "tanstack-start-manifest:v",
+        "tanstack-start-injected-head-scripts:v"
+      ]
+    }
   },
 });
