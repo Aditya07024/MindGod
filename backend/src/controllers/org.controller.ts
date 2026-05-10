448e669d13f033f7653e6d0d93a5deb2edd5bc8c
Aditya07024 <adityakumar07024@gmail.com>
2026-05-10 12:57:19 +0530

    otp fixes

--- backend/src/controllers/org.controller.ts
+++ backend/src/controllers/org.controller.ts
@@ -12,7 +12,8 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || "smtp.gmail.com",
   port: Number(process.env.SMTP_PORT) || 587,
   secure: process.env.SMTP_SECURE === "true", // true for 465, false for other ports
-  auth: {
+  family: 4,
+    auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
